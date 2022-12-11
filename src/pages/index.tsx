073f0a5d277b1d6e53c13380ebfca990e209bb89
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-12-11 15:54:06 -0300

    Add link for product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home";
 import { stripe } from "../lib/stripe";
-import camiseta1 from '../assets/camisetas/1.png' 
-import camiseta2 from '../assets/camisetas/2.png' 
-import camiseta3 from '../assets/camisetas/3.png'
-
-import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe";
+import 'keen-slider/keen-slider.min.css'
+import { styled } from "@stitches/react";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -32,20 +30,22 @@ export default function Home({products}: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return(
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} alt='camiseta' width={520} height={400}/>
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product> 
+          <Link href={`product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt='camiseta' width={520} height={400}/>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product> 
+          </Link>
         )
       })}
     </HomeContainer>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -59,13 +59,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: priceValue,
+      price: new Intl.NumberFormat('pt-br',{
+        style: 'currency',
+        currency: 'BRL'
+      }).format(priceValue),
     }
   })
 
   return {
     props:{ 
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
