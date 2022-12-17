3f868771ce6f1c1be234bfc199b3ae0359f5afb8
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-12-17 18:51:37 -0300

    feat: Create components Product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,80</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores architecto obcaecati maxime itaque rem exercitationem quos reprehenderit, inventore veniam nisi nulla vero molestiae assumenda laudantium voluptas dolore nesciunt illo expedita.</p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
