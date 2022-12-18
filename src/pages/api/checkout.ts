deaaace5e0e24f6a3909fb982c269bcdb036e924
Leonardo Eugenio Mascarenhas Mantoni <51415831+leonardoEugenio@users.noreply.github.com>
2022-12-18 19:59:38 -0300

    Feat: Error Method not allowed

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -4,6 +4,10 @@ import { stripe } from "../../lib/stripe";
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     const { priceId } = req.body
 
+    if (req.method != 'POST') {
+        return res.status(405).json({error: 'Method not allowed'})
+    }
+
     if (!priceId) {
         return res.status(403).json({ error: 'price not found.' })
     }
